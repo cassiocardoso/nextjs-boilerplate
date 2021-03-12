1261e7f4613b06b5918d801e21e2ca3374df9c98
Cassio Cardoso <cassio.cardoso@vimcar.com>
2021-03-12 15:10:27 +0100

    feat(jest): add eslint integration and jest-styled-components
    use the eslint integration to provide sensible defaults for the jest configuration. use
    jest-styled-components to generate better snapshots with the css output

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
 };
